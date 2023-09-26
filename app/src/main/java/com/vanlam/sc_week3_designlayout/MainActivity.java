afde3789b38e3bcaf8a2dc63990a204074945fae
VanLamTruong <lamtruong3242@gmail.com>
2023-09-26 18:38:45 +0700

    upload project

---
+++ app/src/main/java/com/vanlam/sc_week3_designlayout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vanlam.sc_week3_designlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView btnSignIn = (ImageView) findViewById(R.id.button_signIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/vanlam/sc_week3_designlayout/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.vanlam.sc_week3_designlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
